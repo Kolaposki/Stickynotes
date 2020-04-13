e692f4a7f68649befd69fb0a49713badbb51422f
Oshodi Kolapo <oshodikolapo@gmail.com>
2020-04-13 14:38:49 +0100

    Add dummy field to update form in template

--- stickynotes/forms.py
+++ stickynotes/forms.py
@@ -28,13 +28,13 @@ class NoteForm2(forms.ModelForm):
     description = forms.CharField(required=False, help_text="coming from form 2 update",
                                   widget=forms.TextInput(attrs={'id': 'new_description'}))
 
-    # input_type: 'hidden'
     background_color = forms.CharField(max_length=10, required=True, strip=True,
                                        help_text="coming from form 2 update",
                                        widget=forms.TextInput(attrs={'id': 'new_bgcolor'}))
 
     is_done = forms.BooleanField(required=False, help_text="coming from form 2 update",
                                  widget=forms.TextInput(attrs={'id': 'new_is_done'}))
+
     update_dummy = forms.CharField(required=False)
 
     class Meta:
