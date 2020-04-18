72016b68c10514894aeb1ed0084420a73cf971a8
Oshodi Kolapo <oshodikolapo@gmail.com>
2020-04-18 10:14:48 +0100

    Create separate functions for crud operations on note

--- stickynotes/urls.py
+++ stickynotes/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    # path('', views.create_note, name='create'),
+    # path('', views.update_note, name='update'),
+    # path('', views.delete_note, name='delete'),
+
+
     path("register/", views.register, name="register"),
     path('', include("django.contrib.auth.urls")),  # for authentication purposes
 ]
--- stickynotes/views.py
+++ stickynotes/views.py
@@ -6,12 +6,12 @@ from django.http import QueryDict
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+HOMEPAGE = 'home.html'
+
 
 # ALL CRUD FUNCTIONALITIES FOR NOTE
 def index(request):
-    form = NoteForm()
-    up_form = NoteForm2(request.POST)
-    notes = Note.objects.all().order_by('-date_added')
+    notes = None
 
     # Getting note
     if request.method == 'GET':
@@ -101,8 +101,97 @@ def index(request):
 
         return JsonResponse(data)
 
-    return render(request, 'home.html',
-                  context={'form': form, 'notes': notes, "up_form": up_form})
+    return render(request, HOMEPAGE, context={'notes': notes})
+
+
+'''
+def create_note(request):
+    form = NoteForm()
+
+    if 'new_dummy' in request.POST:
+        print("THIS REQUEST IS FROM NEW DUMMY")
+        form = NoteForm(request.POST)
+
+        if form.is_valid():
+            instance = form.save(commit=False)  # get the form but dont save in db yet
+            instance.manager = request.user
+            username = request.user.username
+            print("USER who created note is: ", username)
+            note_form = form.save()
+            note_pk = note_form.pk
+
+            title = request.POST.get('title')
+            description = request.POST.get('description')
+            background_color = request.POST.get('background_color')
+            is_done = False
+            date_added = timezone.now()
+
+            data = {}
+            data['message'] = 'form note is created'
+            data['title'] = title
+            data['description'] = description
+            data['background_color'] = background_color
+            data['note_pk'] = note_pk
+            data['username'] = username
+            data['is_done'] = is_done
+            data['date_added'] = date_added
+
+            print("Created note for ", note_pk)
+            return JsonResponse(data)
+
+    return render(request, HOMEPAGE, context={'form': form})
+
+
+def update_note(request):
+    up_form = NoteForm2(request.POST)
+
+    if request.method == 'POST':
+        print("REQUEST IS POST")
+        if 'update_delete_dummy' in request.POST:
+            print("update_delete_dummy")
+            note_id = request.POST.get('note_id')
+            print("NOTED-ID: ", note_id)
+
+            obj = get_object_or_404(Note, id=note_id)
+            print(f"Here is object to update: {obj}")
+            up_form = NoteForm2(request.POST or None, instance=obj)
+
+            if up_form.is_valid():
+                print("UPDATE FORM IS VALID")
+
+                data = {}
+                title = request.POST.get('title')
+                up_form.save()
+                print(f"Updated Note: {obj}")
+
+                data['message'] = 'form note is updated'
+                data['note_pk'] = note_id
+                data['title'] = title
+                return JsonResponse(data)
+
+    return render(request, HOMEPAGE, context={'up_form': up_form})
+
+
+def delete_note(request):
+    if request.method == 'DELETE':
+        print("REQUEST IS DELETE")
+        note_id = int(QueryDict(request.body).get('note_id'))
+        print("NOTED-ID: ", note_id)
+        obj = get_object_or_404(Note, id=note_id)
+        print(f"Here is object to delete: {obj}")
+
+        data = {}
+        data['message'] = 'Note successfully deleted'
+        data['note_pk'] = note_id
+        data['title'] = obj.title
+
+        obj.delete()
+        print("NOTE DELETED")
+
+        return JsonResponse(data)
+
+    return render(request, HOMEPAGE)
+'''
 
 
 def register(response):
