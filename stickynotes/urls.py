c391b967841faf3a4002aefabe14788394158185
Oshodi Kolapo <oshodikolapo@gmail.com>
2020-04-18 21:46:28 +0100

    Create demo user for testing and redesign logout page

--- stickynotes/urls.py
+++ stickynotes/urls.py
@@ -4,14 +4,8 @@ from django.contrib.auth import views as auth_views  # Use django inbuilt authen
 
 urlpatterns = [
     path('', views.index, name='home'),
-    # path('', views.create_note, name='create'),
-    # path('', views.update_note, name='update'),
-    # path('', views.delete_note, name='delete'),
-
-
     path("register/", views.register, name="register"),
     path('', include("django.contrib.auth.urls")),  # for authentication purposes
-
     path('login', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
